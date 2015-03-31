4f27817eb4df5ac860a762d6514f2fd6945fe294
elvismdev <elvismdev@gmail.com>
2015-03-31 00:34:00 -0400

    Adding columns to the usage description

--- woocommerce-products-by-custom-tax.php
+++ woocommerce-products-by-custom-tax.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce - Display Products by Custom Tax
  * Plugin URI: https://github.com/elvismdev/woocommerce-products-by-custom-tax
- * Description: List WooCommerce products by a custom taxonomy type for products using a shortcode, ex: [woo_products_custom_tax tax_name="vendor" tax_tags="apple,samsung"]
+ * Description: List WooCommerce products by a custom taxonomy type for products using a shortcode, ex: [woo_products_custom_tax tax_name="vendor" tax_tags="apple,samsung" columns="4"]
  * Version: 1.0
  * Author: Elvis Morales
  * Author URI: https://twitter.com/n3rdh4ck3r
@@ -13,55 +13,50 @@
 /*
  * List WooCommerce Products by custom taxonomy
  *
- * ex: [woo_products_custom_tax tax_name="vendor" tax_tags="apple,samsung"]
+ * ex: [woo_products_custom_tax tax_name="vendor" tax_tags="apple,samsung" columns="4"]
  */
 function woo_products_by_custom_tax_shortcode( $atts ) {
 	global $woocommerce_loop;
 
 	if ( empty( $atts ) ) return '';
 
-	// Get attributes
 	extract(shortcode_atts(array(
 		'columns' 	=> '4',
 		'tax_name' => '',
 		'tax_tags' => ''
-	), $atts));
+		), $atts));
 
 	if ( $tax_name === '' || $tax_tags === '' ) return '';
 
 	ob_start();
 
-	// Define Query Arguments
 	$args = array(
-				'post_type' => 'product',
-				$tax_name => $tax_tags
-				);
+		'post_type' => 'product',
+		$tax_name => $tax_tags
+		);
 
-	// Create the new query
 	$products = new WP_Query( $args );
 
 	$woocommerce_loop['columns'] = $columns;
 
 
-	// If results
 	if( $products->have_posts() ) :
 
 		woocommerce_product_loop_start();
 
-			// Start the loop
-			while ( $products->have_posts() ) : $products->the_post();
+	while ( $products->have_posts() ) : $products->the_post();
 
-				wc_get_template_part( 'content', 'product' );
+	wc_get_template_part( 'content', 'product' );
 
-			endwhile;
+	endwhile;
 
-		woocommerce_product_loop_end();
+	woocommerce_product_loop_end();
 
 	else :
 
 		_e('No product matching your criteria.');
 
-	endif; // endif $product_count > 0
+	endif;
 
 	wp_reset_postdata();
 
